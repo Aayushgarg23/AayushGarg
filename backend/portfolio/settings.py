e6311ed3f2a30b825af9e3cffeeebc7fe7c3f644
Aayush Garg <142904158+Aayushgarg23@users.noreply.github.com>
2025-05-26 23:31:47 +0530

    Update settings.py

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -3,6 +3,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email settings
+#for sending mail to personal email along with admin
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
